29085d4e646e6be6b54d08e30572655bb2356e14
Jens Nolte <jens@nightmarestudio.de>
2019-11-14 18:41:38 +0100

    Change default config to non-personalized path

--- src/QBar/DefaultConfig.hs
+++ src/QBar/DefaultConfig.hs
@@ -6,18 +6,21 @@ import QBar.Core
 import Control.Concurrent.Async
 import Pipes
 
+blockLocation :: String -> FilePath
+blockLocation name = "~/.config/qbar/blocks/" <> name
+
 generateDefaultBarConfig :: BarUpdateChannel -> IO [BlockProducer]
 generateDefaultBarConfig barUpdateChannel = do
   (systemInfoInterval, systemInfoIntervalTask) <- sharedInterval barUpdateChannel 10
   link systemInfoIntervalTask
-  --let irc = (systemInfoInterval $ blockScript "/home/jens/run/blocks/irc")
-  let todo = (systemInfoInterval $ blockScript "/home/jens/run/blocks/todo")
-  let wifi = (systemInfoInterval $ blockScript "/home/jens/run/blocks/wifi2 wlan") >-> modify (addIcon "📡")
-  let networkEnvironment = (systemInfoInterval $ blockScript "/home/jens/run/blocks/network-environment")
-  let cpu = (systemInfoInterval $ blockScript "/home/jens/run/blocks/cpu_usage") >-> modify (setBlockName "cpu" . addIcon "💻") >-> autoPadding
-  let ram = (systemInfoInterval $ blockScript "/home/jens/run/blocks/memory") >-> modify (addIcon "🐏") >-> autoPadding
-  let temperature = (systemInfoInterval $ blockScript "/home/jens/run/blocks/temperature") >-> autoPadding
-  let volumeBlock = startPersistentBlockScript barUpdateChannel "/home/jens/run/blocks/volume-pulseaudio -S -F3"
-  let battery = (systemInfoInterval $ blockScript "/home/jens/run/blocks/battery2")
+  --let irc = (systemInfoInterval $ blockScript "irc")
+  let todo = (systemInfoInterval $ blockScript $ blockLocation "todo")
+  let wifi = (systemInfoInterval $ blockScript $ blockLocation "wifi2 wlan") >-> modify (addIcon "📡")
+  let networkEnvironment = (systemInfoInterval $ blockScript $ blockLocation "network-environment")
+  let cpu = (systemInfoInterval $ blockScript $ blockLocation "cpu_usage") >-> modify (setBlockName "cpu" . addIcon "💻") >-> autoPadding
+  let ram = (systemInfoInterval $ blockScript $ blockLocation "memory") >-> modify (addIcon "🐏") >-> autoPadding
+  let temperature = (systemInfoInterval $ blockScript $ blockLocation "temperature") >-> autoPadding
+  let volumeBlock = startPersistentBlockScript barUpdateChannel $ blockLocation "volume-pulseaudio -S -F3"
+  let battery = (systemInfoInterval $ blockScript $ blockLocation "battery2")
   let date = dateBlockProducer barUpdateChannel
   return [todo, wifi, networkEnvironment, cpu, ram, temperature, volumeBlock, battery, date]
