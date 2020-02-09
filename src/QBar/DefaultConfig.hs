e28f4a8edfee1eb5b682e4097f93b7a995cf6358
Jens Nolte <jens@nightmarestudio.de>
2020-02-10 00:49:05 +0100

    Modify blocks before caching in default config
    This is better for performance - should not really be measurable but the
    example config should lead with a good example.

--- src/QBar/DefaultConfig.hs
+++ src/QBar/DefaultConfig.hs
@@ -16,10 +16,10 @@ generateDefaultBarConfig = do
   systemInfoInterval <- sharedInterval 10
 
   let todo = systemInfoInterval (blockScript $ blockLocation "todo")
-  let wifi = systemInfoInterval (blockScript $ blockLocation "wifi2") >-> modify (addIcon "📡\xFE0E")
+  let wifi = systemInfoInterval $ (blockScript $ blockLocation "wifi2") >-> modify (addIcon "📡\xFE0E")
   let networkEnvironment = systemInfoInterval (blockScript $ blockLocation "network-environment")
-  let ram = systemInfoInterval (blockScript $ blockLocation "memory") >-> modify (addIcon "🐏\xFE0E") >-> autoPadding
-  let temperature = systemInfoInterval (blockScript $ blockLocation "temperature") >-> autoPadding
+  let ram = systemInfoInterval $ (blockScript $ blockLocation "memory") >-> modify (addIcon "🐏\xFE0E") >-> autoPadding
+  let temperature = systemInfoInterval $ (blockScript $ blockLocation "temperature") >-> autoPadding
   let volumeBlock = persistentBlockScript $ blockLocation "volume-pulseaudio -S -F3"
   let battery = systemInfoInterval $ batteryBlock >-> modify (blockName ?~ "battery")
   let cpuUsage = systemInfoInterval $ cpuUsageBlock 1 >-> modify ((blockName ?~ "cpuUsage") . addIcon "💻\xFE0E")
