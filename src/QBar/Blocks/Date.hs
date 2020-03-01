275d699bce2deee0b9342d4314f68e2642ee5788
Jens Nolte <jens@nightmarestudio.de>
2020-03-01 18:55:11 +0100

    Use new timer infrastructure for date block

--- src/QBar/Blocks/Date.hs
+++ src/QBar/Blocks/Date.hs
@@ -7,19 +7,12 @@ import QBar.Time
 import qualified Data.Text.Lazy as T
 import Data.Time.Format
 import Data.Time.LocalTime
-import Control.Lens
 
 
 dateBlock :: PushBlock
-dateBlock = forever $ do
-  updateBlock =<< liftIO dateBlockOutput
-  sleepUntilInterval everyMinute
-
-
-dateBlockOutput :: IO BlockOutput
-dateBlockOutput = do
-  zonedTime <- getZonedTime
+dateBlock = schedulePullBlock' (everyNSeconds 60) $ forever $ do
+  zonedTime <- liftIO getZonedTime
   let date = T.pack (formatTime defaultTimeLocale "%a %F" zonedTime)
   let time = T.pack (formatTime defaultTimeLocale "%R" zonedTime)
   let text = normalText ("📅\xFE0E " <> date <> " ") <> activeText time
-  return $ blockName ?~ "date" $ mkBlockOutput text
+  updateBlock $ mkBlockOutput text
