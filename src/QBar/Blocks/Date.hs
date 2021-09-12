d3f3e6ee5d5587cdd9f6ad2b87e12651e78248bc
Jens Nolte <git@queezle.net>
2021-09-12 20:52:30 +0200

    Add short output variant to date block
    The full date is very long compared to other blocks, which is
    problematic when using qbar on devices with small or vertical screens
    (e.g. on the Pinephone).

--- src/QBar/Blocks/Date.hs
+++ src/QBar/Blocks/Date.hs
@@ -13,7 +13,9 @@ import Data.Time.LocalTime
 dateBlock :: Block
 dateBlock = runPollBlock' (everyNSeconds 60) $ forever $ do
   zonedTime <- liftIO getZonedTime
+  let logo = "📅\xFE0E "
   let date = T.pack (formatTime defaultTimeLocale "%a %F" zonedTime)
   let time = T.pack (formatTime defaultTimeLocale "%R" zonedTime)
-  let text = normalText ("📅\xFE0E " <> date <> " ") <> activeText time
-  yieldBlockUpdate $ mkBlockOutput text
+  let text = normalText (logo <> date <> " ") <> activeText time
+  let short = normalText logo <> activeText time
+  yieldBlockUpdate $ (mkBlockOutput text) { _shortText = Just short }
