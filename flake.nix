f429baaa4a1a87f29628c7fd4fbee96de3c03970
J. Konrad Tegtmeier-Rottach <jktr@0x16.de>
2023-05-01 14:17:40 +0200

    Bump project layout

--- flake.nix
+++ flake.nix
@@ -1,44 +1,65 @@
 {
-  outputs = { self, nixpkgs }: with nixpkgs.lib; let
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
 
+  outputs = { self, nixpkgs }:
+  with nixpkgs.lib;
+  let
     systems = platforms.unix;
-    forAllSystems = f: genAttrs systems (system: f system);
-
+    forAllSystems = fn: (genAttrs systems (system:
+      fn (import nixpkgs {
+        inherit system;
+        overlays = [
+          self.overlays.default
+        ];
+      })
+    ));
+    getHaskellPackages = pkgs: pattern: pipe pkgs.haskell.packages [
+      attrNames
+      (filter (x: !isNull (strings.match pattern x)))
+      (sort (x: y: x>y))
+      (map (x: pkgs.haskell.packages.${x}))
+      head
+    ];
   in {
+    packages = forAllSystems (pkgs: let
+      ghc92 = getHaskellPackages pkgs "ghc92.";
+    in rec {
+      default = qbar;
+      qbar = ghc92.qbar;
+    });
 
-    packages = forAllSystems (system:
-      let pkgs = import nixpkgs { inherit system; overlays = [ self.overlay ]; };
-      in rec {
-        default = qbar;
-        qbar = pkgs.haskellPackages.qbar;
-      }
-    );
+    overlays = {
+      default = final: prev: {
+        haskell = prev.haskell // {
+          packageOverrides = hfinal: hprev: prev.haskell.packageOverrides hfinal hprev // {
+            qbar = hfinal.callCabal2nix "qbar" ./qbar {};
 
-    defaultPackage = forAllSystems (system: self.packages.${system}.qbar);
-
-    overlay = final: prev: {
-      haskell = prev.haskell // {
-        packageOverrides = hfinal: hprev: prev.haskell.packageOverrides hfinal hprev // {
-          qbar = import ./. {
-            pkgs = final;
-            haskellPackages = hfinal;
+            # https://gitlab.haskell.org/ghc/ghc/-/issues/22425
+            ListLike = final.haskell.lib.dontCheck hprev.ListLike;
           };
         };
       };
     };
 
-    devShell = forAllSystems (system:
+    devShells = forAllSystems (pkgs:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in pkgs.mkShell {
-        inputsFrom = [ self.packages.${system}.default.env ];
-        packages = [
-          pkgs.cabal-install
-          pkgs.zsh
-          pkgs.entr
-          pkgs.ghcid
-          pkgs.haskell-language-server
-        ];
+        haskellPackages = getHaskellPackages pkgs "ghc92.";
+      in rec {
+        default = haskellPackages.shellFor {
+          packages = hpkgs: [
+            hpkgs.qbar
+          ];
+          nativeBuildInputs = [
+            haskellPackages.haskell-language-server
+            pkgs.cabal-install
+            pkgs.hlint
+
+            # in addition, for ghcid-wrapper
+            pkgs.zsh
+            pkgs.entr
+            pkgs.ghcid
+          ];
+        };
       }
     );
 
